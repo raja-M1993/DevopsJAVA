c97de644dac212b00c33d2b2cf644bccbaa30c05
Raja <raja.m@mstsolutions.com>
2017-05-09 16:15:30 +0530

    Commit with release

---
+++ src/main/java/com/mst/DisplayData.java
@@ -0,0 +1,49 @@
+package com.mst;
+
+
+import java.io.Serializable;
+
+/**
+ * Servlet implementation class DisplayData
+ */
+public class DisplayData implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private String stname,rollno,course,fees;
+	public String getName()
+	{
+		return stname;
+	}
+	public void setName(String stname)
+	{
+		this.stname=stname;
+	}
+	public String getRollno()
+	{
+		return rollno;
+	}
+	public void setRollno(String rollno)
+	{
+		this.rollno=rollno;
+	}
+	public String getCourse()
+	{
+		return course;
+	}
+	public void setCourse(String course)
+	{
+		this.course=course;
+	}
+	public String getFees()
+	{
+		return fees;
+	}
+	public void setFees(String fees)
+	{
+		this.fees=fees;
+	}
+    public DisplayData() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
